faf62c8d35178b705f97c0c479aca4bfe2120c2f
Mike Griffith <bikegriffith@gmail.com>
2015-08-01 15:29:03 -0400

    strip out code i plan to write live

--- static/js/app.js
+++ static/js/app.js
@@ -15,42 +15,4 @@
               });
   });
 
-  // Main controller
-  todoApp.controller('TodoListCtrl', function(TodoResource) {
-    var self = this;
-
-    var init = function() {
-      TodoResource.query(function(todos) {
-        self.todos = _.groupBy(todos, 'status');
-      });
-    };
-    init();
-
-    self.changeStatus = function(todo, status) {
-      todo.status = status;
-      TodoResource.update({id: todo.id}, todo, function(todo) {
-        init();
-      });
-    };
-
-    self.show = function(todo) {
-      self.todo = todo;
-    };
-  });
-
-  // <todo-list ... /> componenet
-  todoApp.directive('todoList', function() {
-    return {
-      scope: {
-          todos: '=todos',
-          nextStatus: '=nextStatus',
-          nextStatusLabel: '=nextStatusLabel',
-          btnColor: '=btnColor',
-          onSelect: '=onSelect',
-          onChangeStatus: '=onChangeStatus'
-      },
-      templateUrl: '/static/partials/todo-list.html'
-    };
-  });
-
 })();
--- static/js/app.test.js
+++ static/js/app.test.js
@@ -9,29 +9,7 @@ describe('todo list', function() {
     expect(countCss('.unassigned li')).toEqual(5);
   });
 
-  it('should show details for a todos', function() {
-    clickFirst('.unassigned a')
-    expect(countCss('.details')).toEqual(1);
-  });
 
-  it('should let me close the details view', function() {
-    clickFirst('.unassigned a')
-    element(by.css('.details button')).click();
-    expect(countCss('.details')).toEqual(0);
-  });
-
-  it('should let a button move to the doing column', function() {
-    expect(countCss('.assigned li')).toEqual(0);
-    clickFirst('.unassigned button')
-    expect(countCss('.assigned li')).toEqual(1);
-  });
-
-  it('should let a button move to the done column', function() {
-    expect(countCss('.done li')).toEqual(0);
-    clickFirst('.unassigned button');
-    clickFirst('.assigned button');
-    expect(countCss('.done li')).toEqual(1);
-  });
 });
 
 
